6ca7f6638f8326a8d5504e8b9487392d04d43e82
Administrator <1429460318@qq.com>
2016-05-30 18:37:56 +0800

    使用 HTML 与 CSS 实现类似 BootStrap 的响应式 12 栏网格布局，根据屏幕宽度，元素占的栏数不同

---
+++ waterfall-ajax/test.php
@@ -0,0 +1,11 @@
+<?php
+header("Content-Type:application/json;charset=utf-8");
+echo('{"data": [
+{"src": "1.jpg"},
+{"src": "2.jpg"},
+{"src": "3.jpg"},
+{"src": "4.jpg"},
+{"src": "5.jpg"},
+{"src": "6.jpg"}
+]}');
+?>
